1b226e8f6e13b76700272e9777fb1d91ba5fd636
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2023-08-19 16:35:21 -0400

    first hello

--- greeting/greet.c
+++ greeting/greet.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("Hello, your handsome beast!");
+  } else {
+    printf("Hello, %s\n", argv[1]);
+  }
 
   return 0;
 }
